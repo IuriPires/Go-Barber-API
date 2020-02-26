69540fa0403d0d2633a5feab6e2352a99cbd2033
iuri.pires <iuripires.work@gmail.com>
2020-02-26 09:34:12 -0300

    first steps

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import User from './app/models/Users'
+
+const routes = new Router();
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Iuri Pires da Rocha',
+    email: 'iuripires.work@gmail.com',
+    'password_hash': '12373847'
+  })
+
+  return res.json(user);
+});
+
+export default routes;
